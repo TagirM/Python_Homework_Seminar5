97b1bd18e8c1a0acee3b7afd87fedbb8757cb623
Admin <tmurzagaleev@yandex.ru>
2022-10-12 23:18:10 +0700

    Решение задачи 4 из ДЗ №5

--- Zadacha4.py
+++ Zadacha4.py
@@ -3,26 +3,39 @@
 # На сжатие входные данные: WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
 # Выходные данные:          12W1B12W3B24W1B14W
 
-
-data1 = open('file_with_data.txt', 'r')
-myData = []
-for line in data1:
-    myData.extend(line)
-data1.close()
-# print(myData)
-count = 1
+# сжатие данных
+data1 = open('file_with_inDataDeployed.txt', 'r')
+count=1
 myNewData = []
-for index in range(1, len(myData)):
-    if myData[index-1] == myData[index] and index != len(myData)-1:
-        count += 1
-    elif myData[index-1] != myData[index] and index != len(myData)-1:
-        myNewData.append(count)
-        myNewData.append(myData[index-1])
-        count = 1
-    elif index == len(myData)-1:
-        count += 1
-        myNewData.append(count)
-        myNewData.append(myData[index])
+for line in data1:
+    line = list(line)
+for index in range(1, len(line)):
+        if line[index-1]==line[index] and index != len(line)-1:
+            count+=1
+        elif line[index-1]!=line[index] and index != len(line)-1:
+            myNewData.append(count)
+            myNewData.append(line[index-1])
+            count=1
+        elif index == len(line)-1:
+            count += 1
+            myNewData.append(count)
+            myNewData.append(line[index])
+data1.close()
 # print(''.join(map(str,myNewData)))
-with open('file_with_newdata.txt', 'w') as data2:
+with open('file_with_outDataCompression.txt', 'w') as data2:
     data2.write(''.join(map(str, myNewData)))
+
+# восстановление данных
+data3 = open('file_with_inDataCompression.txt', 'r')
+myNewData = []
+for line in data3:
+    line = list(line)
+for index in range(1, len(line)):
+    if line[index-1].isdigit() and not line[index].isdigit():
+        myNewData.append(int(line[index-1])*line[index])
+    elif line[index-1].isdigit() and line[index].isdigit():
+        line[index]= line[index-1] + line[index]
+data3.close()
+# print(''.join(map(str,myNewData)))
+with open('file_with_outDataDeployed.txt', 'w') as data4:
+    data4.write(''.join(map(str, myNewData)))
